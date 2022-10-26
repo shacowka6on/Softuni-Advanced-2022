5ea1d4c609ebacf8da9b3c8813d0b2ca3c1061cd
sashi <92218265+shacowka6on@users.noreply.github.com>
2022-10-26 17:49:44 +0300

    Add files via upload

---
+++ 01. Lab/01. Stacks and Queues/2. Stack Sum/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace Advanced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack<char>();
+            string input = Console.ReadLine();
+            foreach (var item in input)
+            {
+                stack.Push(item);
+            }
+            Console.WriteLine(string.Join("", stack));
+        }
+    }
+}
