01d58fe343410f9b0fd9c517f41a20249ce1b475
sashi <92218265+shacowka6on@users.noreply.github.com>
2022-10-27 21:02:42 +0300

    Add files via upload

---
+++ 01. Lab/01. Stacks and Queues/08. Traffic Jam/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace Advanced
+{
+    class Program
+    {
+        static void Maain(string[] args)
+        {
+            int carsThatCanPass = int.Parse(Console.ReadLine());
+            var traffic = new Queue<string>();
+            int count = 0;
+            while (true)
+            {
+                string cmd = Console.ReadLine();
+                if (cmd == "end") break;
+
+                if (cmd == "green")
+                {
+                    if (carsThatCanPass > traffic.Count)
+                    {
+                        carsThatCanPass = traffic.Count;
+                    }
+                    for (int i = 0; i < carsThatCanPass; i++)
+                    {
+                        Console.WriteLine($"{traffic.Dequeue()} passed!");
+                        count++;
+                    }
+                }
+                else
+                {
+                    traffic.Enqueue(cmd);
+                }
+            }
+            Console.WriteLine($"{count} cars passed the crossroads.");
+        }
+    }
+}
