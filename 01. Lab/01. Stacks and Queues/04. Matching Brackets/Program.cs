7a0918345048ccd14dcc163b1324f649684ab5b2
sashi <92218265+shacowka6on@users.noreply.github.com>
2022-10-27 21:00:47 +0300

    Add files via upload

---
+++ 01. Lab/01. Stacks and Queues/04. Matching Brackets/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace Advanced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var indexes = new Stack<int>();
+            string expression = Console.ReadLine();
+            for (int i = 0; i < expression.Length; i++)
+            {
+                if (expression[i] == '(')
+                {
+                    indexes.Push(i);
+                }
+                else if (expression[i] == ')')
+                {
+                    int startIndex = indexes.Pop();
+                    Console.WriteLine(expression.Substring(startIndex, i - startIndex + 1));
+                }
+            }
+        }
+    }
+}
