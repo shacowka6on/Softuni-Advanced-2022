627c9b6f4701974bf34439113d52709c8994ab92
sashi <92218265+shacowka6on@users.noreply.github.com>
2022-10-27 21:01:42 +0300

    Add files via upload

---
+++ 01. Lab/01. Stacks and Queues/06. Supermarket/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace Advanced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var queue = new Queue<string>();
+            while (true)
+            {
+                string cmd = Console.ReadLine();
+                if (cmd == "End") break;
+
+                if (cmd == "Paid")
+                {
+                    int n = queue.Count;
+                    for (int i = 0; i < n; i++)
+                    {
+                        Console.WriteLine(queue.Dequeue());
+                    }
+                }
+                else
+                {
+                    queue.Enqueue(cmd);
+                }
+            }
+            if (queue.Count > 0)
+            {
+                Console.WriteLine($"{queue.Count} people remaining.");
+            }
+        }
+    }
+}
