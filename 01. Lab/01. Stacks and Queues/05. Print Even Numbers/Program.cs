37f6dd817da0327c845965e65c9927c8c13729d7
sashi <92218265+shacowka6on@users.noreply.github.com>
2022-10-27 21:01:16 +0300

    Add files via upload

---
+++ 01. Lab/01. Stacks and Queues/05. Print Even Numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace Advanced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var queue = new Queue<int>();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    queue.Enqueue(numbers[i]);
+                }
+            }
+            Console.WriteLine(String.Join(", ", queue));
+        }
+    }
+}
