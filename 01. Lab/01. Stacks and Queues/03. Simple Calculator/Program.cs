1556b398967cb090a33c71959664b9846b8888e4
sashi <92218265+shacowka6on@users.noreply.github.com>
2022-10-27 21:00:20 +0300

    Add files via upload

---
+++ 01. Lab/01. Stacks and Queues/03. Simple Calculator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace Advanced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack<string>();
+            string[] expression = Console.ReadLine().Split();
+            for (int i = 0; i < expression.Length; i++)
+            {
+                stack.Push(expression[i]);
+                if (stack.Count == 3)
+                {
+                    int first = int.Parse(stack.Pop());
+                    var op = stack.Pop();
+                    int second = int.Parse(stack.Pop());
+                    int result = 0;
+                    if (op == "+")
+                    {
+                        result = first + second;
+                    }
+                    else
+                    {
+                        result = Math.Abs(first - second);
+                    }
+                    stack.Push(result.ToString());
+                }
+            }
+            Console.WriteLine(stack.Pop());
+        }
+    }
+}
