0dd713ca3b33e266665b7e1dfa6f85d6ceeaa671
sashi <92218265+shacowka6on@users.noreply.github.com>
2022-10-27 21:02:19 +0300

    Add files via upload

---
+++ 01. Lab/01. Stacks and Queues/07. Hot Potato/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace Advanced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] kids = Console.ReadLine().Split();
+            int n = int.Parse(Console.ReadLine());
+
+            var queue = new Queue<string>(kids);
+            int toss = 1;
+            while (queue.Count > 1)
+            {
+                string kid = queue.Dequeue();
+                if (toss == n)
+                {
+                    Console.WriteLine($"Removed {kid}");
+                    toss = 1;
+                    continue;
+                }
+                queue.Enqueue(kid);
+                toss++;
+            }
+            Console.WriteLine($"Last is {queue.Dequeue()}");
+        }
+    }
+}
