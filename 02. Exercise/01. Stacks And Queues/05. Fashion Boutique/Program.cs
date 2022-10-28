cbbc22d8dc49f06ed4fe5a0f27459d5c89a1a72f
sashi <92218265+shacowka6on@users.noreply.github.com>
2022-10-28 18:35:49 +0300

    Add files via upload

---
+++ 02. Exercise/01. Stacks And Queues/05. Fashion Boutique/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Advanced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] clothes = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries) 
+                .Select(int.Parse)
+                .ToArray();
+            var stack = new Stack<int>(clothes);
+            int capacity = int.Parse(Console.ReadLine());
+            int racks = 1;
+            int sum = 0;
+
+            while (stack.Count > 0)
+            {
+                sum += stack.Peek();
+                if (sum <= capacity)
+                {
+                    stack.Pop();
+                }
+                else
+                {
+                    sum = 0;
+                    racks++;
+                }
+            }
+            Console.WriteLine(racks);
+        }
+    }
+}
