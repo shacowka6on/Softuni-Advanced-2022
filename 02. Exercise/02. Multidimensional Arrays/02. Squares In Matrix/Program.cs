e25c9c11f1a9261c2636c4c6d0833f9212822f7d
sashi <92218265+shacowka6on@users.noreply.github.com>
2022-11-01 16:48:45 +0200

    Add files via upload

---
+++ 02. Exercise/02. Multidimensional Arrays/02. Squares In Matrix/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Advanced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] size = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            string[,] matrix = new string[size[0], size[1]];
+
+            int matching = 0;
+            FillMatrix(matrix);
+            for (int row = 0; row < matrix.GetLength(0) - 1; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 1; col++)
+                {
+                    if (matrix[row, col] == matrix[row + 1, col] &&
+                        matrix[row, col] == matrix[row, col + 1] &&
+                        matrix[row, col] == matrix[row + 1, col + 1])
+                    {
+                        matching++;
+                    }
+                }
+            }
+            Console.WriteLine(matching);
+        }
+
+        static void FillMatrix(string[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                string[] s = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = s[col];
+                }
+            }
+        }
+    }
+}
